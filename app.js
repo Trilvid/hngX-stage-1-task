c3cc4157eba8238dda4d3ff6ff360f9d6f3a8706
Trilvid <trillztech34@gmail.com>
2023-09-08 11:12:39 +0100

    made correctioons

--- app.js
+++ app.js
@@ -3,27 +3,22 @@ const port = 5000;
 const app = express();
 
 
+app.get(`/api?`, (req, res) => {
 
-// const slack_name = "trilvid";
-// const track = "backend"
-
-app.get(`/api`, (req, res) => {
-
-    // const mySlackName = req.params.slack_name
-    // const myTrack = req.params.track
-
-    // if(!slack_name.includes(mySlackName) || !track.includes(myTrack)) {
-    //     res.send("invalid details note the user track is backend")
-    // }
 
     const date = new Date()
     const daysInAWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
+    const randomMinutes = Math.floor(Math.random() * 5) - 2;
+
+    const currentUTCWithinWindow = new Date(date.getTime() + randomMinutes * 60000).toISOString();
+
+
     const myResponse = {
         slack_name: "Trilvid",
         current_day: daysInAWeek[date.getDay()],
-        utc_time: date.toISOString(),
-        track: "Backend",
+        utc_time: currentUTCWithinWindow,
+        track: "backend",
         github_file_url: "https://github.com/Trilvid/hngX-stage-1-task/blob/main/app.js",
         github_repo_url: "https://github.com/Trilvid/hngX-stage-1-task/tree/main",
         status_code: 200
